84526f32ef8c451242752dfaa8975971e0b033d1
Renata Graciano Nicolait <re.nicolait@gmail.com>
2026-05-13 15:21:14 -0300

    Primeiro commit do desafio Super Trunfo organizado

---
+++ incremento_decremento.c
@@ -0,0 +1,12 @@
+#include <stdio.h>;
+
+int main;
+
+/*
+Incremento (++)
+Pré-Incremento ++a
+Pós-Incremento a++
+Decremento (--)
+Pré-Decremento --a
+pós-Decremento a--
+*/
--- super_trunfo.c
+++ super_trunfo.c
@@ -7,7 +7,9 @@ char codigo1[50]; //Código da carta
 char cidade1[100]; //Nome da cidade
 int populacao1; //Número de habitantes
 float area1; //Área em km da cidade
-float pib1; //Produto Interno Bruto da cidade
+double pib1; //Produto Interno Bruto da cidade
+float densidade_populacional1; //Calcular a densidade populacional
+float pib_percapta1; //Calcular o pib per capta da cidade
 int turistico1; //Número de pontos turísticos da cidade
 
 //Variáveis carta 2
@@ -16,7 +18,9 @@ char codigo2[50]; //Código da carta
 char cidade2[100]; //Nome da cidade
 int populacao2; //Número de habitantes
 float area2; //Área em km da cidade
-float pib2; //Produto Interno Bruto da cidade
+double pib2; //Produto Interno Bruto da cidade
+float densidade_populacional2; //Calcular a densidade populacional
+float pib_percapta2; //Calcular o pib per capta da cidade
 int turistico2; //Número de pontos turísticos da cidade
 
 //Comandos carta 1
@@ -31,9 +35,13 @@ scanf(" %d", &populacao1);
 printf("Área: ");
 scanf(" %f", &area1);
 printf("PIB: ");
-scanf(" %f", &pib1);
+scanf(" %lf", &pib1);
 printf("Pontos turísticos da cidade: ");
 scanf(" %d", &turistico1);
+printf("A densidade populacional é: %.2f hab/km\n",populacao1/area1);
+printf("O PIB per capta é: %.4f reais\n ",pib1/populacao1);
+densidade_populacional1 = (float)populacao1 / area1;
+pib_percapta1 = pib1 / (float)populacao1; 
 
 //Comandos carta 2
 printf("Estado: ");
@@ -47,9 +55,13 @@ scanf(" %d", &populacao2);
 printf("Área: ");
 scanf(" %f", &area2);
 printf("PIB: ");
-scanf(" %f", &pib2);
+scanf(" %lf", &pib2);
 printf("Pontos turísticos da cidade: ");
 scanf(" %d", &turistico2);
+printf("A densidade populacional é: %.2f hab/km\n",populacao2/area2);
+printf("O PIB per capta é: %.4f reais\n ",pib2/populacao2);
+densidade_populacional2 = (float)populacao2 / area2;
+pib_percapta2 = pib2 / (float)populacao2;
 
 printf("\n--- CARTA 1 ---\n");
 printf("Estado: %c - Código: %s\n", estado1, codigo1);
@@ -57,6 +69,8 @@ printf("Cidade: %s\n", cidade1);
 printf("População: %d\n", populacao1);
 printf("Área: %.2f km2\n", area1); // O %.2f deixará o número bonito
 printf("PIB: %.2f bilhões\n", pib1);
+printf("Densidade Populacional: %.2f hab/km\n",densidade_populacional1);
+printf("PIB per capta: %.4f reais\n",pib_percapta1);
 
 printf("\n--- CARTA 2 ---\n");
 printf("Estado: %c - Código: %s\n", estado2, codigo2);
@@ -64,4 +78,7 @@ printf("Cidade: %s\n", cidade2);
 printf("População: %d\n", populacao2);
 printf("Área: %.2f km2\n", area2);
 printf("PIB: %.2f bilhões\n", pib2);
+printf("Densidade Populacional: %.2f hab/km\n",densidade_populacional2);
+printf("PIB per capta: %.4f reais\n",pib_percapta2);
 }
+
