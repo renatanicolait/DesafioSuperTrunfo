2f40a5c958904d278b1e3c78933ed49d3b7ccf83
Renata Graciano Nicolait <re.nicolait@gmail.com>
2026-05-14 14:24:52 -0300

    Adicionando apenas o arquivo super_ trunfo.c

--- super_trunfo.c
+++ super_trunfo.c
@@ -2,26 +2,26 @@
 int main() {
 
 //Variáveis carta 1
-char estado1; //Nome do estado
-char codigo1[50]; //Código da carta
-char cidade1[100]; //Nome da cidade
-int populacao1; //Número de habitantes
-float area1; //Área em km da cidade
-double pib1; //Produto Interno Bruto da cidade
-float densidade_populacional1; //Calcular a densidade populacional
+char estado1; //Nome do estado Rio Grande do Sul A
+char codigo1[50]; //Código da carta A01
+char cidade1[100]; //Nome da cidade Porto Alegre
+int populacao1; //Número de habitantes 12325000
+float area1; //Área em km da cidade 152111
+double pib1; //Produto Interno Bruto da cidade 69928
+float densidade_populacional1; //Calcular a densidade populacional 
 float pib_percapta1; //Calcular o pib per capta da cidade
-int turistico1; //Número de pontos turísticos da cidade
+int turistico1; //Número de pontos turísticos da cidade 50
 
 //Variáveis carta 2
-char estado2; //Nome do estado
-char codigo2[50]; //Código da carta
-char cidade2[100]; //Nome da cidade
-int populacao2; //Número de habitantes
-float area2; //Área em km da cidade
-double pib2; //Produto Interno Bruto da cidade
+char estado2; //Nome do estado Paraná B
+char codigo2[50]; //Código da carta B02
+char cidade2[100]; //Nome da cidade Curitiba
+int populacao2; //Número de habitantes 6748000
+float area2; //Área em km da cidade 120025
+double pib2; //Produto Interno Bruto da cidade 30050
 float densidade_populacional2; //Calcular a densidade populacional
 float pib_percapta2; //Calcular o pib per capta da cidade
-int turistico2; //Número de pontos turísticos da cidade
+int turistico2; //Número de pontos turísticos da cidade 30
 
 //Comandos carta 1
 printf("Estado: ");
@@ -42,6 +42,7 @@ printf("A densidade populacional é: %.2f hab/km\n",populacao1/area1);
 printf("O PIB per capta é: %.4f reais\n ",pib1/populacao1);
 densidade_populacional1 = (float)populacao1 / area1;
 pib_percapta1 = pib1 / (float)populacao1; 
+while (getchar() != '\n');
 
 //Comandos carta 2
 printf("Estado: ");
@@ -59,7 +60,7 @@ scanf(" %lf", &pib2);
 printf("Pontos turísticos da cidade: ");
 scanf(" %d", &turistico2);
 printf("A densidade populacional é: %.2f hab/km\n",populacao2/area2);
-printf("O PIB per capta é: %.4f reais\n ",pib2/populacao2);
+printf("O PIB per capta é: %.4lf reais\n ",pib2/populacao2);
 densidade_populacional2 = (float)populacao2 / area2;
 pib_percapta2 = pib2 / (float)populacao2;
 
@@ -80,5 +81,39 @@ printf("Área: %.2f km2\n", area2);
 printf("PIB: %.2f bilhões\n", pib2);
 printf("Densidade Populacional: %.2f hab/km\n",densidade_populacional2);
 printf("PIB per capta: %.4f reais\n",pib_percapta2);
+
+if (populacao1 > populacao2) {
+    printf("Carta 1 venceu! População 1: %d | População 2: %d\n",populacao1, populacao2);
 }
+else {
+    printf("Carta 2 venceu! População 1: %d | População 2: %d\n",populacao1, populacao2);
+}
+
+if (area1 > area2) {
+    printf("Carta 1 venceu! Área 1: %f | Área 2: %f\n",area1, area2);
+}
+else {
+    printf("Carta 2 venceu! Área 1: %f | Área 2: %f\n",area1, area2);
+}
+if (pib1 > pib2) {
+    printf("Carta 1 venceu! PIB 1: %lf | PIB 2: %lf\n",pib1, pib2);
+}
+else {
+    printf("Carta 2 venceu! PIB 1: %lf | PIB 2: %lf\n",pib1, pib2);
+}
+if (densidade_populacional1 < densidade_populacional2) {
+    printf("Carta 1 venceu! DP 1: %f | DP 2: %f\n",densidade_populacional1, densidade_populacional2);
+}
+else {
+    printf("Carta 2 venceu! DP 1: %f | DP 2: %f\n",densidade_populacional1, densidade_populacional2);
+}
+if (pib_percapta1 > pib_percapta2) {
+    printf("Carta 1 venceu! PPC 1: %f | PPC 2: %f\n",pib_percapta1, pib_percapta2);
+}
+else {
+    printf("Carta 2 venceu! PPC 1: %f | PPC 2: %f\n",pib_percapta1, pib_percapta2);
+}
+}
+
+
 
